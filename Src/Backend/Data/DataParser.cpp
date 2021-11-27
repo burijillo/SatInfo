c5db21913f3961748131c5ca752440b048a9c099
burijillo <burijillo@gmail.com>
2021-11-27 16:05:02 +0100

    Add boxcat parameters for parsing

--- Src/Backend/Data/DataParser.cpp
+++ Src/Backend/Data/DataParser.cpp
@@ -2,16 +2,15 @@
 #include <filesystem>
 #include <fstream>
 
-DataParser::DataParser()
-{
+DataParser::DataParser() {
     Logger logger = Logger::GetLogger();
     // Check cookis directory exists
     dataPath = std::filesystem::current_path().string().append("/../../cookies/");
-    if (std::filesystem::is_directory(dataPath)){
+    if(std::filesystem::is_directory(dataPath)) {
         // Iterate through the filenames vector to check which files exists
-        for (std::string item:dataFileNameVec){
+        for(std::string item : dataFileNameVec) {
             std::string dataFileName = dataPath + item;
-            if (checkIfDataFileExists(dataFileName)){
+            if(checkIfDataFileExists(dataFileName)) {
                 existingDataFileNames.push_back(dataFileName);
                 std::string logMsg = "Add " + item + " to dataFile set";
                 logger.CreateMessage(logMsg, IObserver::LOG_TYPE::PARSER);
@@ -24,43 +23,71 @@ DataParser::DataParser()
     }
 }
 
-bool DataParser::parseBoxCat()
-{
+bool DataParser::parseBoxCat() {
     bool result = false;
 
     // Check if BoxCat exists
     std::string boxCatPath = dataPath + "BoxScore.json";
-    if (std::find(existingDataFileNames.begin(), existingDataFileNames.end(), boxCatPath) != existingDataFileNames.end()) {
-        std::ifstream fs(boxCatPath, std::ifstream::in);
+    if(std::find(existingDataFileNames.begin(), existingDataFileNames.end(), boxCatPath) !=
+        existingDataFileNames.end()) {
+        std::ifstream  fs(boxCatPath, std::ifstream::in);
         nlohmann::json jParser;
         jParser = nlohmann::json::parse(fs);
 
-        for (auto it = jParser.begin(); it != jParser.end(); ++it) {
-            if (!it.value().at(boxCatProp[0]).is_null()) {
-                std::string country = it.value().at(boxCatProp[0]);
-                boxCat.setnNewItem(country);
-            }
+        for(const auto &item : jParser.items()) {
+            std::string country;
+            std::string spadoc_cd;
+            std::string orbital_tba;
+            std::string orbital_payload_count;
+            std::string orbital_rocket_count;
+            std::string orbital_debris_count;
+            std::string orbital_total_count;
+            std::string decayed_payload_count;
+            std::string decayed_rocket_count;
+            std::string decayed_debris_count;
+            std::string decayed_total_count;
+            std::string country_total;
+
+            country               = item.value()[boxCatProp[0]].get<std::string>();
+            spadoc_cd             = item.value()[boxCatProp[1]].get<std::string>();
+            orbital_tba           = item.value()[boxCatProp[2]].get<std::string>();
+            orbital_payload_count = item.value()[boxCatProp[3]].get<std::string>();
+            orbital_rocket_count  = item.value()[boxCatProp[4]].get<std::string>();
+            orbital_debris_count  = item.value()[boxCatProp[5]].get<std::string>();
+            orbital_total_count   = item.value()[boxCatProp[6]].get<std::string>();
+            decayed_payload_count = item.value()[boxCatProp[7]].get<std::string>();
+            decayed_rocket_count  = item.value()[boxCatProp[8]].get<std::string>();
+            decayed_debris_count  = item.value()[boxCatProp[9]].get<std::string>();
+            decayed_total_count   = item.value()[boxCatProp[10]].get<std::string>();
+            country_total         = item.value()[boxCatProp[11]].get<std::string>();
+
+            boxCat.setnNewItem(country, spadoc_cd, orbital_tba, orbital_payload_count,
+                orbital_rocket_count, orbital_debris_count, orbital_total_count,
+                decayed_payload_count, decayed_rocket_count, decayed_debris_count,
+                decayed_total_count, country_total);
         }
 
-        Logger logger = Logger::GetLogger();
+        Logger      logger     = Logger::GetLogger();
         std::string boxCatSize = "";
-        boxCatSize = "Number of countries/orgs: " + std::to_string(boxCat.getSize());
+        boxCatSize             = "Number of countries/orgs: " + std::to_string(boxCat.getSize());
         logger.CreateMessage(boxCatSize, IObserver::LOG_TYPE::PARSER);
 
         /*std::vector<BoxUnit> boxCatVec = boxCat.getBoxCatVec();
         for (auto item:boxCatVec){
             std::string test = item.country;
-            logger.CreateMessage("COUNTRY ID " + std::to_string(item.id) + " : " + test, IObserver::LOG_TYPE::PARSER);
+            logger.CreateMessage("COUNTRY ID " + std::to_string(item.id) + " : "
+        + test, IObserver::LOG_TYPE::PARSER);
         }*/
-        for (int i = 0; i < boxCat.getSize() + 2; i++){
+        for(int i = 0; i < boxCat.getSize(); i++) {
             BoxUnit boxUnit = boxCat.getBoxUnit(i);
-            if (boxUnit.isValid) {
-                logger.CreateMessage("COUNTRY ID " + std::to_string(boxUnit.id) + " : " + boxUnit.country, IObserver::LOG_TYPE::PARSER);
+            if(boxUnit.isValid) {
+                logger.CreateMessage("COUNTRY " + std::to_string(boxUnit.id) + " : " +
+                                         boxUnit.country + " - " + boxUnit.spadoc_cd,
+                    IObserver::LOG_TYPE::PARSER);
             } else {
                 logger.CreateMessage("ERROR MASEMO", IObserver::LOG_TYPE::SYS_ERR);
             }
         }
-
 
     } else {
         result = false;
@@ -69,11 +96,10 @@ bool DataParser::parseBoxCat()
     return result;
 }
 
-bool DataParser::checkIfDataFileExists(std::string fileName)
-{
+bool DataParser::checkIfDataFileExists(std::string fileName) {
     bool result = false;
 
-    if (std::filesystem::exists(fileName)){
+    if(std::filesystem::exists(fileName)) {
         result = true;
     }
 
--- Src/Backend/Data/DataSets/BoxCat.h
+++ Src/Backend/Data/DataSets/BoxCat.h
@@ -1,35 +1,59 @@
 #pragma once
-#include <string>
-#include <array>
 #include "GenericCat.h"
+#include <array>
+#include <string>
 
-// TODO TRY TO INHERIT FROM COMMONALITIES
-const static std::array<std::string, 2> boxCatProp = {
-    "COUNTRY",
-    "SPADOC_CD"
-};
+const static std::array<std::string, 12> boxCatProp = {"COUNTRY", "SPADOC_CD", "ORBITAL_TBA",
+    "ORBITAL_PAYLOAD_COUNT", "ORBITAL_ROCKET_BODY_COUNT", "ORBITAL_DEBRIS_COUNT",
+    "ORBITAL_TOTAL_COUNT", "DECAYED_PAYLOAD_COUNT", "DECAYED_ROCKET_BODY_COUNT",
+    "DECAYED_DEBRIS_COUNT", "DECAYED_TOTAL_COUNT", "COUNTRY_TOTAL"};
 
-struct BoxUnit : public GenericUnit
-{
+struct BoxUnit : public GenericUnit {
     std::string country;
+    std::string spadoc_cd;
+    std::string orbital_tba;
+    std::string orbital_payload_count;
+    std::string orbital_rocket_count;
+    std::string orbital_debris_count;
+    std::string orbital_total_count;
+    std::string decayed_payload_count;
+    std::string decayed_rocket_count;
+    std::string decayed_debris_count;
+    std::string decayed_total_count;
+    std::string country_total;
 };
 
-class BoxCat : public GenericCat
-{
+class BoxCat : public GenericCat {
 public:
     ~BoxCat() {}
-    void setnNewItem(std::string _country) { 
+
+    void setnNewItem(std::string _country, std::string _spadoc_cd, std::string _orbital_tba,
+        std::string _orbital_payload_count, std::string _orbital_rocket_count,
+        std::string _orbital_debris_count, std::string _orbital_total_count,
+        std::string _decayed_payload_count, std::string _decayed_rocket_count,
+        std::string _decayed_debris_count, std::string _decayed_total_count,
+        std::string _country_total) {
         BoxUnit unit;
-        unit.id = getSize() + 1;
-        unit.country = _country;
-        
+        unit.id                    = getSize() + 1;
+        unit.country               = _country;
+        unit.spadoc_cd             = _spadoc_cd;
+        unit.orbital_tba           = _orbital_tba;
+        unit.orbital_rocket_count  = _orbital_rocket_count;
+        unit.orbital_debris_count  = _orbital_debris_count;
+        unit.orbital_total_count   = _orbital_total_count;
+        unit.decayed_payload_count = _decayed_payload_count;
+        unit.decayed_rocket_count  = _decayed_rocket_count;
+        unit.decayed_debris_count  = _decayed_debris_count;
+        unit.decayed_total_count   = _decayed_total_count;
+        unit.country_total         = _country_total;
+
         BoxCatVec.push_back(unit);
         setSize(static_cast<unsigned int>(BoxCatVec.size()));
     }
 
     BoxUnit getBoxUnit(int index) {
         BoxUnit result;
-        if (index > getSize()){
+        if(index > getSize()) {
             result.isValid = false;
             return result;
         } else {
@@ -41,6 +65,5 @@ public:
     std::vector<BoxUnit> getBoxCatVec() { return BoxCatVec; }
 
 private:
-
     std::vector<BoxUnit> BoxCatVec;
 };
