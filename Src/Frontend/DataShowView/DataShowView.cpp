aac93365ee9672f0b196009ace70e2ead1e246e5
burijillo <burijillo@gmail.com>
2021-12-12 10:50:26 +0100

    Add new layout example

--- Src/Frontend/DataShowView/DataShowView.cpp
+++ Src/Frontend/DataShowView/DataShowView.cpp
@@ -4,19 +4,29 @@ void DataShowView::setDataShowViewCtrl(wxWindow *parent, DataParser *_dataParser
     dataShowViewCtrl = new wxPanel(
         parent, wxID_ANY, wxDefaultPosition, parent->FromDIP(wxSize(220, 90)), wxNO_BORDER);
 
-    wxStaticBox *box2      = new wxStaticBox(dataShowViewCtrl.get(), wxID_ANY,
-        "&Test data.........................................................:");
-    wxSizer *sizerMiddleUp = new wxStaticBoxSizer(box2, wxVERTICAL);
-    wxSizer *sizerMiddle   = new wxBoxSizer(wxVERTICAL);
-    sizerMiddle->Add(sizerMiddleUp, 0, wxGROW);
-    // sizerMiddle->Add(sizerMiddleDown, 1, wxGROW | wxTOP, 5);
+    // TEST
 
-    wxStaticBox *box3    = new wxStaticBox(dataShowViewCtrl.get(), wxID_ANY, "&Text:");
-    wxSizer *m_sizerText = new wxStaticBoxSizer(box3, wxHORIZONTAL);
-    m_sizerText->SetMinSize(150, 0);
-
-    wxSizer *sizerTop = new wxBoxSizer(wxHORIZONTAL);
-    sizerTop->Add(sizerMiddle, 0, wxGROW | wxALL, 10);
-    sizerTop->Add(m_sizerText, 1, wxGROW | (wxALL & ~wxRIGHT), 10);
-    dataShowViewCtrl.get()->SetSizer(sizerTop);
+    wxBoxSizer *topsizer = new wxBoxSizer(wxVERTICAL);
+    // create text ctrl with minimal size 100x60
+    topsizer->Add(new wxTextCtrl(dataShowViewCtrl.get(), -1, "My text.", wxDefaultPosition,
+                      wxSize(100, 60), wxTE_MULTILINE),
+        1,         // make vertically stretchable
+        wxEXPAND | // make horizontally stretchable
+            wxALL, //   and make border all around
+        10);       // set border width to 10
+    wxBoxSizer *button_sizer = new wxBoxSizer(wxHORIZONTAL);
+    button_sizer->Add(new wxButton(dataShowViewCtrl.get(), wxID_OK, "OK"),
+        0,     // make horizontally unstretchable
+        wxALL, // make border all around (implicit top alignment)
+        10);   // set border width to 10
+    button_sizer->Add(new wxButton(dataShowViewCtrl.get(), wxID_CANCEL, "Cancel"),
+        0,     // make horizontally unstretchable
+        wxALL, // make border all around (implicit top alignment)
+        10);   // set border width to 10
+    topsizer->Add(button_sizer,
+        0,                                            // make vertically unstretchable
+        wxALIGN_CENTER);                              // no border and centre horizontally
+    dataShowViewCtrl.get()->SetSizerAndFit(topsizer); // use the sizer for layout and size window
+                                                      // accordingly and prevent it from being
+                                                      // resized to smaller size
 }
